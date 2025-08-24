d1f4470cbe88496d16bc3f1751aa2f7cdde11f8f
DorianQA-WEB <qa.doriangrey@gmail.com>
2025-08-24 22:01:35 +0500

    Initial commit

---
+++ test_example.py
@@ -0,0 +1,18 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Ожидаем, что заголовок "содержит" подстроку
+    expect(page).to_have_title(re.compile("Playwright"))
+
+def test_get_started_link(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Клик на ожидаемую, на странице ссылку
+    page.get_by_role("link", name="Get started").click()
+
+    # Ожидаем, что на странице будет заголовок с именем Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
