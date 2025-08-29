0f3abf4d284419e7622d8faacb0645be409c8ac1
DorianQA-WEB <qa.doriangrey@gmail.com>
2025-08-29 20:29:42 +0500

    Codegen

---
+++ test_codegen.py
@@ -0,0 +1,8 @@
+from playwright.async_api import Page
+
+
+def test_add_todo(page: Page):
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_placeholder("What needs to be done?").click()
+    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
